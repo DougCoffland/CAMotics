79a1871fb5c3f5d6b3d6c19cba4f8b0b94b6d238
Doug Coffland <dougcoffland@gmail.com>
2019-02-17 14:09:27 -0800

    added log message

--- src/gcode/plan/LineCommand.cpp
+++ src/gcode/plan/LineCommand.cpp
@@ -67,6 +67,7 @@ bool LineCommand::merge(const LineCommand &lc, const PlannerConfig &config,
   const double a = length;
   const double b = lc.length;
 
+LOG_INFO(1,"theta = " << theta);
   // Don't merge lines that point in the opposite direction
   if (isnan(theta) || 3.14 < theta) return false;
 
