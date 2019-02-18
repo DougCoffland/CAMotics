9e431907dcc6c5015f543c3383dd0d0a53d77d60
Doug Coffland <dougcoffland@gmail.com>
2019-02-17 18:28:10 -0800

    changed error messages in linecommand.cpp

--- src/gcode/plan/LineCommand.cpp
+++ src/gcode/plan/LineCommand.cpp
@@ -66,8 +66,8 @@ bool LineCommand::merge(const LineCommand &lc, const PlannerConfig &config,
   const double theta = unit.angleBetween(lc.unit);
   const double a = length;
   const double b = lc.length;
-LOG_INFO(1, "theta: " << theta << " unit: " << unit << " lc.unit: " << lc.unit);
-LOG_INFO(1,"\ntarget = " << target << " lc.target = " << lc.target);
+LOG_INFO(1,"\ntheta: " << theta << " unit: " << unit << " lc.unit: " << lc.unit);
+LOG_INFO(1,"target = " << target << " lc.target = " << lc.target);
 LOG_INFO(1,"a = " << a << " b = " << b << " theta = " << theta);
   // Don't merge lines that point in the opposite direction
   if (isnan(theta) || 3.14 < theta) return false;
